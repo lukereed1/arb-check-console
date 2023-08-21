d7e4d9fd2909b1ef12c90539b96e7e992552efec
Luke Reed <luke.reed@outlook.com.au>
2023-08-22 09:46:08 +1000

    Added reset array function so that if user scraped multiple sports in one session they wouldn't get duplicate data each time they run scraper

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const readline = require("readline");
 const axios = require("axios");
 const configPath = path.join(__dirname, "config.json");
 const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
-const API_URL = config.API_URL;
 
+const API_URL = config.API_URL;
 const allGames = [];
 const bestMargins = [];
 const selectedBooks = ["neds", "sportsbet", "unibet", "pointsbet", "tab"];
@@ -26,15 +26,15 @@ async function main() {
 			clearInterval(loading);
 			clearLoadingDots();
 			findGameStats();
-			main();
-			break;
+			resetArrays();
+			await main();
 		case "2":
 			await importBookieDataForChosenSport("rugby-league");
 			clearInterval(loading);
 			clearLoadingDots();
 			findGameStats();
-			main();
-			break;
+			resetArrays();
+			await main();
 		case "3":
 			await importAllSports();
 			/* Afl must be sorted due to some games being at the
@@ -42,74 +42,16 @@ async function main() {
 			sortTeamsList(allGames[0]);
 			clearInterval(loading);
 			clearLoadingDots();
-			findGameStats();
-			main();
-			break;
+			findGameStats(); // Prints to console
+			resetArrays();
+			await main();
 		case "4":
 			process.exit(0);
 			break;
 		default:
-			console.log("Invalid input.\n");
-			main();
+			console.log("Invalid input\n");
+			await main();
 			break;
-	}
-}
-
-async function getInput(prompt) {
-	return new Promise((resolve) => {
-		rl.question(prompt, (answer) => {
-			resolve(answer);
-		});
-	});
-}
-
-function findGameStats() {
-	allGames.forEach((sport) => compareSelectedBooks(sport));
-	findMarginPercentage(bestMargins);
-	bestMargins.sort((a, b) => a.margin - b.margin);
-	console.log(bestMargins);
-}
-
-function findMarginPercentage(games) {
-	for (let i = 0; i < games.length; i++) {
-		let margin =
-			1 / games[i].firstTeamBestOdds + 1 / games[i].secondTeamBestOdds;
-
-		let roundedMargin = (margin * 100).toFixed(2);
-		bestMargins[i].margin = roundedMargin;
-	}
-}
-
-function compareSelectedBooks(sport) {
-	for (let k = 0; k < sport[0].length; k++) {
-		let firstTeamBestOdds = 0;
-		let secondTeamBestOdds = 0;
-		let firstTeamBookieWithBestOdds;
-		let secondTeamBookieWithBestOdds;
-		let firstTeamPlaying;
-		let secondTeamPlaying;
-		for (let i = 0; i < sport.length; i++) {
-			firstTeamPlaying = sport[i][k].firstTeam;
-			secondTeamPlaying = sport[i][k].secondTeam;
-			if (sport[i][k].firstTeamOdds >= firstTeamBestOdds) {
-				firstTeamBestOdds = sport[i][k].firstTeamOdds;
-				firstTeamBookieWithBestOdds = sport[i][k].bookie;
-			}
-
-			if (sport[i][k].secondTeamOdds >= secondTeamBestOdds) {
-				secondTeamBestOdds = sport[i][k].secondTeamOdds;
-				secondTeamBookieWithBestOdds = sport[i][k].bookie;
-			}
-		}
-		const gameData = {
-			game: `${firstTeamPlaying} vs ${secondTeamPlaying}`,
-			firstTeamBestOdds: firstTeamBestOdds,
-			firstTeamBookie: firstTeamBookieWithBestOdds,
-			secondTeamBestOdds: secondTeamBestOdds,
-			secondTeamBookie: secondTeamBookieWithBestOdds,
-		};
-
-		bestMargins.push(gameData);
 	}
 }
 
@@ -145,10 +87,75 @@ async function getGames(bookie, sport) {
 	}
 }
 
+function compareSelectedBooks(sport) {
+	for (let k = 0; k < sport[0].length; k++) {
+		let firstTeamBestOdds = 0;
+		let secondTeamBestOdds = 0;
+		let firstTeamBookieWithBestOdds;
+		let secondTeamBookieWithBestOdds;
+		let firstTeamPlaying;
+		let secondTeamPlaying;
+		for (let i = 0; i < sport.length; i++) {
+			firstTeamPlaying = sport[i][k].firstTeam;
+			secondTeamPlaying = sport[i][k].secondTeam;
+
+			if (sport[i][k].firstTeamOdds >= firstTeamBestOdds) {
+				firstTeamBestOdds = sport[i][k].firstTeamOdds;
+				firstTeamBookieWithBestOdds = sport[i][k].bookie;
+			}
+
+			if (sport[i][k].secondTeamOdds >= secondTeamBestOdds) {
+				secondTeamBestOdds = sport[i][k].secondTeamOdds;
+				secondTeamBookieWithBestOdds = sport[i][k].bookie;
+			}
+		}
+		const gameData = {
+			game: `${firstTeamPlaying} vs ${secondTeamPlaying}`,
+			firstTeamBestOdds: firstTeamBestOdds,
+			firstTeamBookie: firstTeamBookieWithBestOdds,
+			secondTeamBestOdds: secondTeamBestOdds,
+			secondTeamBookie: secondTeamBookieWithBestOdds,
+		};
+
+		bestMargins.push(gameData);
+	}
+}
+
+function findGameStats() {
+	allGames.forEach((sport) => compareSelectedBooks(sport));
+	findMarginPercentage(bestMargins);
+	bestMargins.sort((a, b) => a.margin - b.margin);
+	console.log(bestMargins);
+}
+
+function findMarginPercentage(games) {
+	for (let i = 0; i < games.length; i++) {
+		let margin =
+			1 / games[i].firstTeamBestOdds + 1 / games[i].secondTeamBestOdds;
+
+		let roundedMargin = (margin * 100).toFixed(2);
+		bestMargins[i].margin = roundedMargin;
+	}
+}
+
 function trimGamesArrayLength(array, desiredLength) {
 	for (let i = 0; i < array.length; i++) {
 		if (array[i].length > desiredLength) array[i].splice(desiredLength);
 	}
+}
+
+function sortTeamsList(array) {
+	for (let i = 0; i < array.length; i++) {
+		array[i].sort((a, b) => a.firstTeam.localeCompare(b.firstTeam));
+	}
+}
+
+async function getInput(prompt) {
+	return new Promise((resolve) => {
+		rl.question(prompt, (answer) => {
+			resolve(answer);
+		});
+	});
 }
 
 function showLoadingDots() {
@@ -163,14 +170,13 @@ function showLoadingDots() {
 	}, 500);
 }
 
-function sortTeamsList(array) {
-	for (let i = 0; i < array.length; i++) {
-		array[i].sort((a, b) => a.firstTeam.localeCompare(b.firstTeam));
-	}
-}
-
 function clearLoadingDots() {
 	process.stdout.write("\r        \r");
+}
+
+function resetArrays() {
+	bestMargins.length = 0;
+	allGames.length = 0;
 }
 
 main();
