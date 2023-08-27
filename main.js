d33647849f8d527914de134105ebf67cdf5a3bdf
Luke Reed <luke.reed@outlook.com.au>
2023-08-27 12:24:44 +1000

    Completed baseball functionality: - Checks if books have teams in different order and if so makes them the same - Currently not functional with tab or betdeluxe - Added function to ensure all bookie arrays have the exact same data as some bookies don't show all games

--- main.js
+++ main.js
@@ -10,17 +10,14 @@ const API_URL = config.API_URL;
 const allGames = [];
 const bestMargins = [];
 const selectedBooks = [
-	"sportsbet",
 	"unibet",
-	"boombet",
-	"palmerbet",
-	"tab",
 	"betdeluxe",
-	"topsport",
 	"neds",
-	"pointsbet",
+	"sportsbet",
+	"boombet",
+	"tab",
 ];
-const allSports = ["afl", "rugby-league"];
+const allSports = ["afl", "rugby-league", "mlb"];
 
 const rl = readline.createInterface({
 	input: process.stdin,
@@ -28,7 +25,9 @@ const rl = readline.createInterface({
 });
 
 async function main() {
-	const userInput = await getInput("1. AFL\n2. NRL\n3. All Sports\n4. Exit\n");
+	const userInput = await getInput(
+		"1. AFL\n2. NRL\n3. MLB\n4. All Sports\n5. Exit\n"
+	);
 	let loading;
 	switch (userInput) {
 		case "1":
@@ -50,6 +49,14 @@ async function main() {
 			await main();
 		case "3":
 			loading = showLoadingDots();
+			await importBookieDataForChosenSport("mlb");
+			clearInterval(loading);
+			clearLoadingDots();
+			findGameStats();
+			resetArrays();
+			await main();
+		case "4":
+			loading = showLoadingDots();
 			await importAllSports();
 			/* Afl must be sorted due to some games being at the
 			 same time and bookies ordering them differently */
@@ -59,7 +66,7 @@ async function main() {
 			findGameStats(); // Prints to console
 			resetArrays();
 			await main();
-		case "4":
+		case "5":
 			process.exit(0);
 		default:
 			console.log("Invalid input\n");
@@ -78,22 +85,59 @@ async function importAllSports() {
 async function importBookieDataForChosenSport(sport) {
 	let lowestAmountOfGames = Infinity;
 
-	let sortedSport = await bluebird.map(
+	let allSportsAllBooksIncludingBooksWithNoGames = await bluebird.map(
 		selectedBooks,
 		async (book) => {
 			let allBookieGames = await getGames(book, sport);
-			if (book.length === 0) console.log(`${book} has no games available`);
 			return allBookieGames;
 		},
 		{ concurrency: 2 }
 	);
 
-	sortedSport.forEach((games) => {
+	// Removes bookie if it is showing no games
+	let sortedSportAllBooks = allSportsAllBooksIncludingBooksWithNoGames.filter(
+		(subArray) => subArray.length > 0
+	);
+
+	// Finds bookie with lowest amount of games
+	sortedSportAllBooks.forEach((games) => {
 		if (games.length < lowestAmountOfGames) lowestAmountOfGames = games.length;
 	});
 
-	allGames.push(sortedSport);
-	trimGamesArrayLength(sortedSport, lowestAmountOfGames);
+	if (sport === "mlb") {
+		// Removes bookies not compatible with MLB at the moment
+		for (let i = 0; i < sortedSportAllBooks.length; i++) {
+			if (
+				sortedSportAllBooks[i][0].bookie === "Tab" ||
+				sortedSportAllBooks[i][0].bookie === "BetDeluxe"
+			) {
+				sortedSportAllBooks.splice(i, 1);
+			}
+		}
+
+		/* MLB team orders always different across books
+		this checks one order and makes the rest the same */
+		let teamOne = sortedSportAllBooks[0][0].firstTeam.split()[0];
+		for (let i = 0; i < sortedSportAllBooks.length; i++) {
+			if (sortedSportAllBooks[i][0].firstTeam.split()[0] !== teamOne) {
+				sortedSportAllBooks[i].forEach((game) => {
+					let tempTeam = game.firstTeam;
+					game.firstTeam = game.secondTeam;
+					game.secondTeam = tempTeam;
+
+					let tempOdds = game.firstTeamOdds;
+					game.firstTeamOdds = game.secondTeamOdds;
+					game.secondTeamOdds = tempOdds;
+				});
+			}
+		}
+		// For MLB some bookies only show some games, this ensures each bookie array has exact same game data
+		sortTeamsList(sortedSportAllBooks);
+		sortedSportAllBooks = ensureBookiesHaveSameGameData(sortedSportAllBooks);
+	}
+
+	trimGamesArrayLength(sortedSportAllBooks, lowestAmountOfGames);
+	allGames.push(sortedSportAllBooks);
 }
 
 async function getGames(bookie, sport) {
@@ -137,6 +181,24 @@ function compareSelectedBooks(sport) {
 
 		bestMargins.push(gameData);
 	}
+}
+
+function ensureBookiesHaveSameGameData(array) {
+	let allTeams = new Set(
+		array.flat().map((obj) => obj.firstTeam.split(" ")[0])
+	);
+
+	allTeams = [...allTeams].filter((team) =>
+		array.every((subArray) =>
+			subArray.some((obj) => obj.firstTeam.startsWith(team))
+		)
+	);
+
+	return array.map((subArray) =>
+		allTeams.map((team) =>
+			subArray.find((obj) => obj.firstTeam.startsWith(team))
+		)
+	);
 }
 
 function findGameStats() {
@@ -195,6 +257,11 @@ function clearLoadingDots() {
 function resetArrays() {
 	bestMargins.length = 0;
 	allGames.length = 0;
+}
+
+async function test() {
+	await importBookieDataForChosenSport("mlb");
+	console.log(allGames);
 }
 
 main();
